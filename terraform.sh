60ea796468dcf50e12f8d3909a6eb6a0692aec6e
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2018-01-21 00:16:26 -0200

    parameters checking

--- terraform.sh
+++ terraform.sh
@@ -4,11 +4,18 @@ PROVIDER=$1
 ACTION=$2
 
 cd $PROVIDER
+if [ $? -ne 0 ]; then
+	echo "Unsupported provider: $PROVIDER"
+	exit 1
+fi
 if [ $ACTION == "create" ]; then
 	terraform init
 	terraform apply -auto-approve
 elif [ $ACTION == "destroy" ]; then
 	terraform destroy -force
+else
+	echo "Unknown action: $ACTION"
+	exit 1
 fi
 
 exit 0
