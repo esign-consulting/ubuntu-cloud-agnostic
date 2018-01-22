4d4ed8215a175e7a2c2db1539928018ce4d91c6d
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2018-01-22 07:01:48 -0200

    public ip

--- aws/ubuntu.tf
+++ aws/ubuntu.tf
@@ -5,4 +5,9 @@ provider "aws" {
 resource "aws_instance" "ubuntu" {
   ami = "ami-1157157d"
   instance_type = "t2.micro"
+  associate_public_ip_address = true
+
+  provisioner "local-exec" {
+    command = "echo '[ubuntu]\n${aws_instance.ubuntu.public_ip}' > hosts"
+  }
 }
